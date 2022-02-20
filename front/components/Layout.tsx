7cd8f0545b1828023eae65ebdf220670a94c32e3
Samy Senani <samy.senani@deepor.ai>
2022-02-20 13:39:40 +0100

    add: layout

---
+++ front/components/Layout.tsx
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Container, Box } from '@chakra-ui/react'
+
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+
+import NavBar from "../components/NavBar"
+import Footer from "../components/Footer"
+
+
+interface ILayout {
+  children: React.ReactNode,
+  isHeaderVisible?: boolean,
+  isFooterVisible?: boolean
+}
+
+export default function Layout({ children, isHeaderVisible, isFooterVisible }: ILayout) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Ecommerce Web App" />
+        <meta name="og:title" content='Ecommerce Web App' />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title> Ecommerce Web App </title>
+      </Head>
+
+      <Container maxW='container.xl' px="0">
+        { isHeaderVisible && <NavBar /> }
+          <Box px="2rem" py="1rem">
+            {children}
+          </Box>
+        { isFooterVisible && <Footer /> }
+      </Container>
+    </>
+  )
+}

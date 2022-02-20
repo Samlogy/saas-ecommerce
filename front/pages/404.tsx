a42d2f59ddf60bcb7a39aec71d883690a2c312b1
Samy Senani <samy.senani@deepor.ai>
2022-02-20 14:53:47 +0100

    upgrade: 404 page

--- front/pages/404.tsx
+++ front/pages/404.tsx
@@ -1,34 +1,24 @@
-import { Heading, Button, Text, ButtonGroup, Container } from "@chakra-ui/react"
+import { Box, Heading, Text, Button } from '@chakra-ui/react';
 import Link from 'next/link'
 
 import Layout from "../components/Layout"
 
-const NotFound = () => {
-  
-  // minHeight="100vh"
+export default function NotFound() {
   return (
-      <Layout> 
-        <Container maxW="80em" py="39px" px={["16px","","","40px"]} m="10vh auto 0 auto" borderRadius="4px"
-                  display="flex" flexDir="column" justifyContent="center" alignItems="center">
-          <Heading as="h1" size="2xl" my="2rem">
-            404 Not Found
-          </Heading>
+    <Layout textAlign="center" py={10} px={6}>
+      <Heading display="inline-block" as="h1" fontSize="90px" bgGradient="linear(to-r, teal.400, teal.600)" backgroundClip="text">
+        404
+      </Heading>
+      <Text fontSize="26px" mt={3} mb={2}>
+        Page Not Found
+      </Text>
+      <Text color={'gray.500'} mb={6} fontSize="18px">
+        The page you're looking for does not seem to exist
+      </Text>
 
-          <Text maxW="60em" m="2rem auto" textAlign="center"> Sorry this web Page do not exist please click in the button bellow to login </Text>
-
-          <ButtonGroup>
-            <Button variant="solid"
-                    p=".5rem" borderRadius="md" bg="#4f46e5" textColor="#fff"> 
-                <Link href="/"> Home </Link>               
-            </Button>
-            <Button variant="outline"
-                    p=".5rem" borderRadius="md" bg="#e0e7ff" textColor="#4f46e5"> 
-              <Link href="/contact"> Contact Us </Link> 
-            </Button>
-          </ButtonGroup>
-        </Container>
-      </Layout>
+      <Button colorScheme="teal" bgGradient="linear(to-r, teal.400, teal.500, teal.600)" color="white" variant="solid">
+        <Link href="/"> Go to Home </Link>
+      </Button>
+    </Layout>
   );
 }
-
-export default NotFound;

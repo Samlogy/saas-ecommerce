82102ab7703a86021f214f276b1f0eb9359f71e6
Samy Senani <samy.senani@deepor.ai>
2022-02-22 10:57:02 +0100

    fix: 404 page layout

--- front/pages/404.tsx
+++ front/pages/404.tsx
@@ -5,7 +5,7 @@ import Layout from "../components/Layout"
 
 export default function NotFound() {
   return (
-    <Layout textAlign="center" py={10} px={6}>
+    <Layout textAlign="center" py={10} px={6} h="100vh" display="flex" justifyContent="center" mt="4rem">
       <Heading display="inline-block" as="h1" fontSize="90px" bgGradient="linear(to-r, teal.400, teal.600)" backgroundClip="text">
         404
       </Heading>

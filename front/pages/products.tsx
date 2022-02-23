e5f525c4f52bc3f0d00a898b06819e135989c612
Samy Senani <samy.senani@deepor.ai>
2022-02-23 11:45:10 +0100

    add: products list page

---
+++ front/pages/products.tsx
@@ -0,0 +1,65 @@
+import { Box, Heading, Text, Button, Flex } from '@chakra-ui/react';
+// import Link from 'next/link'
+
+import { Layout, ProductCard, View, Filter } from "../components"
+
+const data = [
+    {
+        isNew: true,
+        imageURL:
+          'https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80',
+        name: 'Wayfarer Classic',
+        price: 4.5,
+        rating: 4.2,
+        numReviews: 34,
+    },
+    {
+        isNew: true,
+        imageURL:
+          'https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80',
+        name: 'Wayfarer Classic',
+        price: 4.5,
+        rating: 4.2,
+        numReviews: 34,
+    },
+    {
+        isNew: true,
+        imageURL:
+          'https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80',
+        name: 'Wayfarer Classic',
+        price: 4.5,
+        rating: 4.2,
+        numReviews: 34,
+    },
+    {
+        isNew: true,
+        imageURL:
+          'https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80',
+        name: 'Wayfarer Classic',
+        price: 4.5,
+        rating: 4.2,
+        numReviews: 34,
+    }
+]
+
+export default function Products() {
+  return (
+    <Layout isHeaderVisible isFooterVisible>
+      <Heading as="h2"> Products </Heading>
+
+      <Filter />
+
+      <View cond={data.length > 0}>
+        <Text> Product result are: {data.length} </Text>
+
+        <Flex flexDir="row" flexWrap="wrap" justifyContent="space-evenly">
+          { data.map((el: any) => <ProductCard data={el} />) }
+        </Flex>
+      </View>
+
+      <View cond={data.length === 0}>
+        <Text> There is no product with thoes filters </Text>
+      </View>
+    </Layout>
+  );
+}

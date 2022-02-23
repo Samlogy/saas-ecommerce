8e478fb23627ab47b7e732eb3bda09d22a71a7d7
Samy Senani <samy.senani@deepor.ai>
2022-02-23 11:24:50 +0100

    edit: nav bar cpt

--- front/components/NavBar.tsx
+++ front/components/NavBar.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
-// import Link from 'next/link'
+import Link from 'next/link'
+
+import { Logo } from "../public/icons"
 
 
 import { ReactNode } from 'react';
@@ -8,7 +10,7 @@ import {
   Flex,
   Avatar,
   HStack,
-  Link,
+  // Link,
   IconButton,
   Button,
   Menu,
@@ -20,17 +22,28 @@ import {
   useColorModeValue,
   Stack,
 } from '@chakra-ui/react';
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import { AiOutlineMenu, AiOutlineClose, AiOutlineShoppingCart } from 'react-icons/ai';
 
-const Links = ['Home', 'Products', 'Contact Us'];
+const Links = [
+  {
+    link: "/",
+    name: "Home"
+  },
+  {
+    link: "/products",
+    name: "Products"
+  },
+  {
+    link: "/contact",
+    name: "Contact Us"
+  },
+];
 
-const NavLink = ({ children }: { children: ReactNode }) => (
-  <Link px={2} py={1} rounded={'md'} _hover={{
-      textDecoration: 'none',
-      bg: useColorModeValue('gray.200', 'gray.700'),
-    }}
-    href={'#'}>
-    {children}
+const NavLink = ({ children, link }: { children: ReactNode, link: string }) => (
+  <Link href={`/${link}`}>
+    <Box px={2} py={1} rounded={'md'} _hover={{ textDecoration: 'none', bg: useColorModeValue('gray.200', 'gray.700') }}> 
+      {children}
+    </Box>
   </Link>
 );
 
@@ -40,44 +53,33 @@ export default function NavBar() {
   return (
       <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <IconButton
-            size={'md'}
-            icon={isOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
-            aria-label={'Open Menu'}
-            display={{ md: 'none' }}
-            onClick={isOpen ? onClose : onOpen}
+          <IconButton size={'md'} icon={isOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
+            aria-label={'Toggle Menu'} display={{ md: 'none' }} onClick={isOpen ? onClose : onOpen}
           />
+
           <HStack spacing={8} alignItems={'center'}>
-            <Box>Logo</Box>
-            <HStack
-              as={'nav'}
-              spacing={4}
-              display={{ base: 'none', md: 'flex' }}>
-              {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
+            <Box> <Logo /> </Box>
+            <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
+              {Links.map((link: any) => (
+                <NavLink key={link.link} link={link.link}> {link.name} </NavLink>
               ))}
             </HStack>
           </HStack>
+
           <Flex alignItems={'center'}>
+            <IconButton aria-label='Shopping Cart' icon={<AiOutlineShoppingCart size={24} />} mr="1rem" />
             <Menu>
-              <MenuButton
-                as={Button}
-                rounded={'full'}
-                variant={'link'}
-                cursor={'pointer'}
-                minW={0}>
-                <Avatar
-                  size={'sm'}
+              <MenuButton as={Button} rounded={'full'} variant={'link'} cursor={'pointer'} minW={0}>
+                <Avatar size={'sm'}
                   src={
                     'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
                   }
                 />
               </MenuButton>
               <MenuList>
-                <MenuItem>Link 1</MenuItem>
-                <MenuItem>Link 2</MenuItem>
+                <MenuItem> My Account </MenuItem>
                 <MenuDivider />
-                <MenuItem>Link 3</MenuItem>
+                <MenuItem> Logout </MenuItem>
               </MenuList>
             </Menu>
           </Flex>
@@ -86,8 +88,8 @@ export default function NavBar() {
         {isOpen ? (
           <Box pb={4} display={{ md: 'none' }}>
             <Stack as={'nav'} spacing={4}>
-              {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
+              {Links.map((link: any) => (
+                <NavLink key={link.link} link={link.link}> {link.name} </NavLink>
               ))}
             </Stack>
           </Box>
@@ -95,3 +97,4 @@ export default function NavBar() {
       </Box>
   );
 }
+

39877a581e4e64fb227d9fed27d937e25350f114
Samy Senani <samy.senani@deepor.ai>
2022-02-24 15:31:59 +0100

    add: data  validation schemas --> register - login - reset - forgot password - contact

--- front/lib/validation/index.ts
+++ front/lib/validation/index.ts
@@ -1,8 +1,31 @@
 import * as yup from 'yup';
-// import { t } from "@lang/i18n";
-
 
 
 export const forgotPasswordSchema = yup.object().shape({
-    email: yup.string().email("Enter a valid Email Address").required("Email Address Required")
+    email: yup.string().email("Enter a valid Email Address").required("Email Address required")
 });
+
+export const resetPasswordSchema = yup.object().shape({
+    password: yup.string().min(8, "Password is at least 8 charasters long").required("Password is required"),                  
+    confirm_password: yup.string().min(8, "Password is at least 8 charasters long").required("Password is required"),                   
+}); 
+
+export const registerSchema = yup.object().shape({  
+    fullName: yup.string().required("Full Name required"),
+    email: yup.string().email("Enter a valid Email Address").required("Email Address required"),
+    password: yup.string().min(8, "Password is at least 8 charasters long").required("Password is required"),
+    confirm_password: yup.string().min(8, "passwords do not match").required(" confirm password required")
+});
+
+export const loginSchema = yup.object().shape({
+    email: yup.string().email("Enter a valid Email Address").required("Email Address required"),
+    password: yup.string().required("Password is required"),
+});
+
+export const contactSchema = yup.object().shape({
+    fullName: yup.string().required("Full Name required"),
+    email: yup.string().email("Enter a valid Email Address").required("Email Address required"),
+    message: yup.string().required("Message Address required"),
+});
+
+// add - edit --> product

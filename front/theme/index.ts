649343833e557e63539b5ebf85019866c3396fea
Samy Senani <samy.senani@deepor.ai>
2022-03-01 08:37:53 +0100

    add: config file --> chakra ui

---
+++ front/theme/index.ts
@@ -0,0 +1,20 @@
+import { extendTheme } from "@chakra-ui/react";
+
+import { colors } from "./utilities"
+
+
+const theme = extendTheme({
+    colors,
+    sizes: {
+        container: {
+            sm: "375px",
+            md: "1024px",
+            lg: "1280px",
+            xl: "1920px",
+            "2xl": "2160px",
+        }
+    }
+});
+
+
+export default theme;
---
+++ front/theme/utilities.ts
@@ -0,0 +1,33 @@
+
+
+export const colors = {
+    white: "#fff",
+    black: "#000",
+    accent: "#57980B",
+
+    // green: {
+    //     main: "#73C316",
+    //     light: "#9CDE50",
+    //     dark: "#57980B",
+    //     radial: "radial-gradient(70.84% 70.84% at 39.77% 35.37%, #99EC51 0%, #429321 100%)"
+    // },
+    // blue: {
+    //     main: "#00466E",
+    //     light: "#0D7AB6",
+    //     dark: "rgba(0, 70, 110, 0.9)",
+    //     radial: "radial-gradient(92.92% 91.44% at 56.95% 35.52%, #0078BB 0%, #003A5B 100%)"
+    // },
+    // error: "#EB5757",
+    // edit: "#F2994A",
+    // gray: {
+    //     light: 'silver',
+    //     borders: "#888",
+    //     disabledIcon: "#bbb",
+    //     borderBoxes: "#6C6C6C",
+    //     hintText: "rgba(152, 152, 152, 0.75)",
+    //     bgApp: "#eee",
+    //     inactiveText: "rgba(255, 255, 255, 0.75)",
+    //     annonce: "#E3E3E3",
+    // },
+    // gradient: "linear-gradient(90deg, #00466E 0%, #1A5E65 18.75%, #8CC63F 100%)"
+};

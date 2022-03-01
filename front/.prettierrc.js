f880a57e3d4b117bb259dc7896683bcf3bcf57d9
Samy Senani <samy.senani@deepor.ai>
2022-03-01 08:21:42 +0100

    upgrade: nodejs to 16.14.0 add: eslint/prettier to project setup

---
+++ front/.prettierrc.js
@@ -0,0 +1,21 @@
+// module.exports = {
+//     tabWidth: 2,
+//     printWidth: 80,
+//     endOfLine: 'auto',
+//     arrowParens: 'avoid',
+//     trailingComma: 'none',
+//     semi: true,
+//     useTabs: false,
+//     singleQuote: true,
+//     bracketSpacing: true
+// };
+module.exports = {
+    semi: false,
+    trailingComma: 'es6',
+    singleQuote: true,
+    printWidth: 100,
+    tabWidth: 2,
+    useTabs: false,
+    jsxBracketSameLine: false,
+    bracketSpacing: true,
+}

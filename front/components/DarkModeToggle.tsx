9af7788efce3b16ee1f6f1e03587cb2b4acd31b0
Samy Senani <samy.senani@deepor.ai>
2022-03-01 08:59:24 +0100

    add: dark mode toggle (cpt + logic)

---
+++ front/components/DarkModeToggle.tsx
@@ -0,0 +1,24 @@
+import { IconButton } from '@chakra-ui/react';
+import { useState } from 'react';
+import { FiMoon, FiSun } from "react-icons/fi"
+
+import { useTheme } from "../store";
+
+const DarkModeToggle = () => {    
+    const mode = useTheme((state: any) => state.mode);
+    const toggleDarkMode = useTheme((state: any) => state.toggleDarkMode);
+
+    const [isDark, setIsDark] = useState(mode);
+
+    const handleChanges = () => {
+        setIsDark((isDark: boolean) => !isDark)
+        toggleDarkMode()
+    };
+    
+    return(
+      <IconButton aria-label='dark mode' bg="transparent" 
+            onClick={() => handleChanges()} 
+            icon={isDark ? <FiMoon /> : <FiSun /> } />
+    )
+}
+export default DarkModeToggle;

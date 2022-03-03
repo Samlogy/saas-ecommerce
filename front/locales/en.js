edd13b75b8a8920c87f7ef3430c958cc57474fef
Samy Senani <samy.senani@deepor.ai>
2022-03-03 09:14:56 +0100

    edit: translate files (fr-eng)

--- front/locales/en.js
+++ front/locales/en.js
@@ -1,4 +1,5 @@
 export default {
+    greeting: "welcome",
     globals: {
         
     },
@@ -8,7 +9,9 @@ export default {
         contact: 'Contact Us',
         login: 'Sign In',
         register: 'Sign Up',
-        logout: 'Sign Out'
+        logout: 'Sign Out',
+        profile: 'Profile',
+        my_account: 'My Account'
     },
     footer: {
         rights: 'All rights reserved',
@@ -20,4 +23,11 @@ export default {
         text: "The page you're looking for does not seem to exist",
         button: 'Go to Home'
     },
+    shopping_cart: {
+        shopping_cart: 'Shopping Cart',
+        remove_all: 'Remove All',
+        msg: 'Shipping and taxes calculated at checkout.',
+        continue_shipping: 'Continue Shipping',
+        checkout: 'Checkout'
+    }
 }
--- front/locales/fr.js
+++ front/locales/fr.js
@@ -1,3 +1,33 @@
 export default {
-    greeting: "Salut !"
+    greeting: "Salut",
+    globals: {
+        
+    },
+    menu: {
+        home: 'Acceuil',
+        products: 'Nos produits',
+        contact: 'Contactez-nous',
+        login: 'Se connecter',
+        register: "S'inscrire",
+        logout: 'Se déconnecter',
+        profile: 'Profile',
+        my_account: 'Mon compte'
+    },
+    footer: {
+        rights: 'Tous droits réservés',
+        conditions: 'Conditions et termes',
+        privacy: 'Confidentialité'
+    },
+    NotFound: {
+        title: "Page non trouvée",
+        text: "La page que vous cherchez ne semble pas exister",
+        button: "Retour à l'acceuil"
+    },
+    shopping_cart: {
+        shopping_cart: 'Panier',
+        remove_all: 'tous supprimés',
+        msg: 'Livraison et taxes calculées à la caisse',
+        continue_shipping: 'Continuer vos achats',
+        checkout: 'Payé'
+    }
 }

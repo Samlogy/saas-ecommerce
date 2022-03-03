0e6b389dbf824f0b5772e660de7090cb5b441a95
Samy Senani <samy.senani@deepor.ai>
2022-03-03 09:02:13 +0100

    add: shopping cart logic (inc - dec - removeOne - removeAll)

--- front/components/ShoppingCart.tsx
+++ front/components/ShoppingCart.tsx
@@ -12,8 +12,9 @@ import {
   import React, { useState } from "react";
 
   import { View } from "../components";
+  import { useShoppingCart } from "../store";
 
-  const  data = [
+  const  products = [
     {
       img: 'https://bit.ly/dan-abramov',
       name: "Throwback Hip Ba",
@@ -40,40 +41,45 @@ import {
     }
   ];
 
-const ShoppingCart = ({ isOpen, onClose }: { isOpen: any, onClose: any }) => {
+const ShoppingCart = () => {
+  const isVisible = useShoppingCart((state: any) => state.isVisible)
+  const total = useShoppingCart((state: any) => state.total)
+  const handleVisibility = useShoppingCart((state: any) => state.handleVisibility)
+  const removeAll = useShoppingCart((state: any) => state.removeAll)
 
-    const onRemoveItem = () => {
-      console.log('remove item')
-    }
   
     return(
-        <Drawer isOpen={isOpen} placement='right' size="sm" onClose={onClose}>
+        <Drawer isOpen={isVisible} placement='right' size="sm" onClose={() => handleVisibility()}>
           <DrawerOverlay />
           <DrawerContent>
             <DrawerCloseButton />
             <DrawerHeader> Shopping Cart </DrawerHeader>
 
             <DrawerBody>
-              <View cond={data.length > 0}>
-                { data.map((el: any) => <CartItem data={el} onRemoveItem={onRemoveItem} />) }
+              <View cond={products.length > 0}>
+                <Text textAlign={'right'} color='blue.500' fontSize='.9rem' mb='1rem' 
+                    _hover={{ cursor: 'pointer', textDecor: 'underline' }}
+                    onClick={() => removeAll()}> Remove all </Text>
+                { products.map((product: any) => <CartItem data={product} />) }
                 <Divider />
 
                 <Flex justifyContent={'space-between'} mt="1rem">
                   <Text fontWeight="600"> Total </Text>
-                  <Text fontWeight="600"> $262.00 </Text>
+                  <Text fontWeight="600"> ${total} </Text>
                 </Flex>
               </View>
-              <View cond={data.length === 0}>
-                <Text> There is not product in your shopping cart </Text>
+
+              <View cond={products.length === 0}>
+                <Text fontSize="1rem" color="gray.500"> There is not product in your shopping cart </Text>
               </View>
             </DrawerBody>
 
             <DrawerFooter display="flex" flexDir="column">
               <Divider my="1rem" />
-              <Text mb="1rem"> Shipping and taxes calculated at checkout. </Text>
+              <Text mb="1rem" fontSize=".9rem" color="gray.500"> Shipping and taxes calculated at checkout. </Text>
               <Button w="full" colorScheme='blue'> Checkout </Button>
               <Text display="flex" mt="1rem"> or 
-                <Text textColor="blue.600" ml="1rem" _hover={{ cursor: "pointer" }}> Continue Shopping → </Text> 
+                <Text textColor="blue.600" ml="1rem" fontSize=".9rem" _hover={{ cursor: "pointer" }}> Continue Shopping → </Text> 
               </Text>
             </DrawerFooter>
           </DrawerContent>
@@ -83,15 +89,26 @@ const ShoppingCart = ({ isOpen, onClose }: { isOpen: any, onClose: any }) => {
 
 export default ShoppingCart;
 
-const CartItem = ({ data, onRemoveItem }: { data: any, onRemoveItem: any }) => {
+const CartItem = ({ data }: { data: any }) => {
   const [quantity, setQuantity] = useState(data.quantity);
 
-  const handleQuantity = (type) => {
+  const increment = useShoppingCart((state: any) => state.increment)
+  const decrement = useShoppingCart((state: any) => state.decrement)
+  const removeOne = useShoppingCart((state: any) => state.removeOne)
+
+  const handleQuantity = (type: string) => {
     if (type === "dec") {
       quantity > 1 && setQuantity(quantity - 1);
+      decrement(data.id)
     } else {
       setQuantity(quantity + 1);
+      increment(data.id)
     }
+  };
+
+  const onRemoveItem = () => {
+    console.log('remove item')
+    removeOne(data.id)
   };
 
   return(

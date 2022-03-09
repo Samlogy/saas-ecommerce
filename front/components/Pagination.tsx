11d62092278ea4f5ee0160dc60327d8169484b2f
Samy Senani <samy.senani@deepor.ai>
2022-03-09 11:35:02 +0100

    add: pagination logic setup: react query in app

--- front/components/Pagination.tsx
+++ front/components/Pagination.tsx
@@ -1,26 +1,37 @@
 import { IconButton, Flex, Button } from '@chakra-ui/react';
 import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
 
-const Pagination = () => {
-    const num = [1, 2, 3]
+const Pagination = ({ page, changePage, pages, nextPage, prevPage, startPage, endPage }) => {
     
     return(
         <Flex flexDir='row' justifyContent={'space-between'} w="60%" m="1.5rem auto">
-            <Button color='gray.600' fontSize='.9rem' mx=".25rem" _hover={{ bg: 'gray.400', color: 'white' }}> Start </Button>
+            <Button color='gray.600' fontSize='.9rem' mx=".25rem" 
+                _hover={{ bg: 'gray.400', color: 'white' }}
+                disabled={page === 1} 
+                onClick={startPage}> Start </Button>
+
             <IconButton aria-label='arrow left' icon={<RiArrowLeftSLine size={22} color='gray' />} 
-               _hover={{ bg: 'gray.300', color: 'white' }}  /> 
+                _hover={{ bg: 'gray.300', color: 'white' }}
+                disabled={page === 1} 
+                onClick={prevPage}  /> 
 
             <Flex flexDir='row' justifyContent={'center'}>
-                {
-                    num.map(el => 
-                    <Button color='gray.600' fontSize='.9rem' mx=".25rem" _hover={{ bg: 'gray.400', color: 'white' }}> {el} </Button>)
+                {   pages.map((el: number) => 
+                    <Button color='gray.600' fontSize='.9rem' mx=".25rem" 
+                            _hover={{ bg: 'gray.400', color: 'white' }}
+                            onClick={() => changePage(el)}> {el} </Button>)
                 }
             </Flex> 
 
             <IconButton aria-label='arrow right' icon={<RiArrowRightSLine size={22} color='gray' />} 
-               _hover={{ bg: 'gray.300', color: 'white' }}  /> 
+                    _hover={{ bg: 'gray.300', color: 'white' }}
+                    disabled={page === 42} 
+                    onClick={nextPage} /> 
 
-            <Button color='gray.600' fontSize='.9rem' mx=".25rem" _hover={{ bg: 'gray.400', color: 'white' }}> Last </Button>
+            <Button color='gray.600' fontSize='.9rem' mx=".25rem" 
+                    _hover={{ bg: 'gray.400', color: 'white' }}
+                    disabled={page === 42} 
+                    onClick={endPage}> Last </Button>
         </Flex>
     )
 }
--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,13 +1,21 @@
 import '../styles/global.css'
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { ReactQueryDevtools } from 'react-query-devtools'
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import theme from "../theme";
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const queryClient = new QueryClient();
+
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        {/* <ReactQueryDevtools initialIsOpen /> */}
+      </QueryClientProvider>
     </ChakraProvider>
   )
 }

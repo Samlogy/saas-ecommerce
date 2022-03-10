11b4a689aaff89a8c2c7b2eced9ed30eb2cce991
Samy Senani <samy.senani@deepor.ai>
2022-03-10 13:36:22 +0100

    add: listing comments cpt

---
+++ front/components/ListingComments.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Flex, Box, Text, Button } from "@chakra-ui/react"
+
+import { Comment, View, AddComment } from "../components"
+
+const comments =[
+    {
+        name: 'Sam',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur provident optio debitis adipisci explicabo',
+        createdAt: '15/03/2022'
+    },
+    {
+        name: 'Sam',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur provident optio debitis adipisci explicabo',
+        createdAt: '15/03/2022'
+    },
+    {
+        name: 'Sam',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur provident optio debitis adipisci explicabo',
+        createdAt: '15/03/2022'
+    },
+];
+
+const ListingComments = () => {
+    const [showAddComment, setShowAddComment] = useState(false);
+
+    return(
+        <Flex flexDir="column">
+            <AddComment isOpen={showAddComment} onClose={() => setShowAddComment(false)} />
+            
+            <Button w="10rem" display={'flex'} ml="auto" onClick={() => setShowAddComment(true)}> Add Comment </Button>
+
+            <View cond={comments.length > 0 }>
+                { comments.map((el: any) =>  <Comment data={el} />) }
+            </View>
+
+            <View cond={comments.length === 0 }>
+                <Text> There's no comment posted yet ! </Text>
+            </View>
+        </Flex>
+    )
+}
+
+export default ListingComments;

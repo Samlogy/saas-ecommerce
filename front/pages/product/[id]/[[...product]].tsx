4f07fc506edd48b7736be85b5e163ae880176b9c
Samy Senani <samy.senani@deepor.ai>
2022-03-14 10:37:59 +0100

    add: catch all routes files inside product folder

---
+++ front/pages/product/[id]/[[...product]].tsx
@@ -0,0 +1,10 @@
+import {useRouter} from "next/router"
+
+export default function catchAllRoutes() {
+    // const router = useRouter()
+    // console.log('router: ', router.query)
+
+    return(
+        <> catch all routes </>
+    )
+}

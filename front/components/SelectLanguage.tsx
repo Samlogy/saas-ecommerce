5076fdfcd288b8786c263740cef7232f3e28b7db
Samy Senani <samy.senani@deepor.ai>
2022-03-16 09:07:49 +0100

    edit: language selector style

--- front/components/SelectLanguage.tsx
+++ front/components/SelectLanguage.tsx
@@ -1,21 +1,27 @@
 import { useRouter } from "next/router";
+import { useColorMode } from '@chakra-ui/react'
 
 import { useLocale } from "../lib/hooks";
+// import lang from "../locales/index.json"
+import { languages } from "../locales"
 
 const SelectLanguage = () => {
     const router = useRouter();
-
+    const { colorMode: mode, toggleColorMode } = useColorMode()
     const { locale } = useLocale()
+    
 
     const changeLanguage = (e: any) => {
       const locale = e.target.value;
       router.push(router.pathname, router.asPath, { locale });
     };
+    const style = mode === "dark" ? "option" : "option active"
 
     return(
       <select onChange={changeLanguage} defaultValue={locale} className='select-language'>
-        <option className="text-black" value="en"> EN </option>
-        <option className="text-black" value="fr"> FR </option>
+        { languages.map((el: string) => 
+          <option className={style} value={el}> {el} </option> 
+        )}
       </select>
     )
 }

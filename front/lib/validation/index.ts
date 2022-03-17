0f717775cfe18363295b7d3e55dae879ce2d5e06
Samy Senani <samy.senani@deepor.ai>
2022-03-17 07:57:54 +0100

    delete: useless schema --> validation

--- front/lib/validation/index.ts
+++ front/lib/validation/index.ts
@@ -1,27 +1,27 @@
 import * as yup from 'yup';
 
 
-export const forgotPasswordSchema = yup.object().shape({
-    email: yup.string().email("Enter a valid Email Address").required("Email Address required")
-});
+// export const forgotPasswordSchema = yup.object().shape({
+//     email: yup.string().email("Enter a valid Email Address").required("Email Address required")
+// });
 
-export const resetPasswordSchema = yup.object().shape({
-    confirm_password: yup.string().min(8, "Password is at least 8 charasters long").required("Password is required").oneOf([yup.ref('password'), null], 'Passwords do not match'),
-    password: yup.string().required("Password is required"),                                     
-}); 
+// export const resetPasswordSchema = yup.object().shape({
+//     confirm_password: yup.string().min(8, "Password is at least 8 charasters long").required("Password is required").oneOf([yup.ref('password'), null], 'Passwords do not match'),
+//     password: yup.string().required("Password is required"),                                     
+// }); 
 
-export const registerSchema = yup.object().shape({  
-    fullName: yup.string().required("Full Name required"),
-    email: yup.string().email("Enter a valid Email Address").required("Email Address required"),
-    password: yup.string().min(8, "Password is at least 8 charasters long").required("Password is required"),
-    confirm_password: yup.string().min(8, "passwords do not match").required(" confirm password required").oneOf([yup.ref('password'), null], 'Passwords do not match'),
-    acceptTerms: yup.bool().oneOf([true], 'Accept Ts & Cs is required')
-});
+// export const registerSchema = yup.object().shape({  
+//     fullName: yup.string().required("Full Name required"),
+//     email: yup.string().email("Enter a valid Email Address").required("Email Address required"),
+//     password: yup.string().min(8, "Password is at least 8 charasters long").required("Password is required"),
+//     confirm_password: yup.string().min(8, "passwords do not match").required(" confirm password required").oneOf([yup.ref('password'), null], 'Passwords do not match'),
+//     acceptTerms: yup.bool().oneOf([true], 'Accept Ts & Cs is required')
+// });
 
-export const loginSchema = yup.object().shape({
-    email: yup.string().email("Enter a valid Email Address").required("Email Address required"),
-    password: yup.string().required("Password is required"),
-});
+// export const loginSchema = yup.object().shape({
+//     email: yup.string().email("Enter a valid Email Address").required("Email Address required"),
+//     password: yup.string().required("Password is required"),
+// });
 
 export const contactSchema = yup.object().shape({
     name: yup.string(),

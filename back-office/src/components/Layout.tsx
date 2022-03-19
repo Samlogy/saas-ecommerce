aa9db6a5fbb8ae72724e8fb711c84bf0eb489ac9
Samy Senani <samy.senani@deepor.ai>
2022-03-19 20:02:17 +0100

    edit: layout cpt logic

--- back-office/src/components/Layout.tsx
+++ back-office/src/components/Layout.tsx
@@ -1,31 +1,27 @@
 import { Container, Flex } from '@chakra-ui/react'
 
+import { SideBar, TopBar } from '../components'
+
 interface ILayout {
   children: React.ReactNode
   isHeaderVisible?: boolean
-  isFooterVisible?: boolean
   rest?: any
 }
 
-export default function Layout({ children, isHeaderVisible, isFooterVisible, ...rest }: ILayout) {
+export default function Layout({ children, isHeaderVisible, ...rest }: ILayout) {
   return (
-    <>
-      <Flex flexDir="column" {...rest}>
-        {/* {isHeaderVisible && <NavBar />} */}
-
-        <Container
-          maxW="1024px"
-          bg="#FFFC"
-          minHeight="calc(100vh - 100px)"
-          py="36px"
-          px={['16px', '', '', '50px', '100px']}
-          borderRadius="4px"
-        >
-          {children}
-        </Container>
-
-        {/* {isFooterVisible && <Footer />} */}
-      </Flex>
-    </>
+    <Flex flexDir="column" {...rest}>
+      {isHeaderVisible && <TopBar />}
+      <Container
+        maxW="1024px"
+        bg="#FFFC"
+        minHeight="calc(100vh - 100px)"
+        py="36px"
+        px={['16px', '', '', '50px', '100px']}
+        borderRadius="4px"
+      >
+        {children}
+      </Container>
+    </Flex>
   )
 }

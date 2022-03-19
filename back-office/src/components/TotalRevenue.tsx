edd40dd8aba310654032414312eff39b9d2269d5
Samy Senani <samy.senani@deepor.ai>
2022-03-19 09:43:32 +0100

    add: back-office widget cpt - total revenue cpt -

---
+++ back-office/src/components/TotalRevenue.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Flex, Text, CircularProgress, CircularProgressLabel, Box } from '@chakra-ui/react'
+
+function TotalRevenue() {
+  return (
+    <Flex
+      flexDir={'column'}
+      justifyContent={'space-between'}
+      alignItems="center"
+      borderColor="gray.400"
+      p=".5rem 1rem"
+      w="30rem"
+      borderRadius={'25px'}
+      boxShadow="md"
+    >
+      <Text textAlign={'left'} fontWeight="600" color={'gray.500'}>
+        Total Revenue
+      </Text>
+
+      <CircularProgress value={40} color="green.400" size="5rem" thickness="10px">
+        <CircularProgressLabel color={'gray.500'} fontSize=".9rem">
+          {' '}
+          40%{' '}
+        </CircularProgressLabel>
+      </CircularProgress>
+
+      <Text color={'gray.400'}>Total Sales made today</Text>
+      <Text fontSize="2rem" fontWeight={'600'}>
+        {' '}
+        $ 420{' '}
+      </Text>
+      <Text color={'gray.400'} fontSize=".9rem" fontWeight={'400'}>
+        Summary of today's transactions
+      </Text>
+
+      <Box></Box>
+    </Flex>
+  )
+}
+
+export default TotalRevenue
---
+++ back-office/src/components/Widget.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Box, Text, Flex, CircularProgress, CircularProgressLabel } from '@chakra-ui/react'
+
+interface IWidget {
+  data?: any
+}
+
+function Widget({ data }: IWidget) {
+  return (
+    <Flex
+      flexDir="row"
+      justifyContent={'space-between'}
+      alignItems="center"
+      //   border="1px solid"
+      borderColor="gray.400"
+      p=".5rem 1rem"
+      w="15em"
+      borderRadius={'25px'}
+      boxShadow="md"
+    >
+      <Flex flexDir="column">
+        <Text> {data.name}: </Text>
+        <Text color={'gray.400'} fontSize=".85rem">
+          {' '}
+          {data.description}:{' '}
+        </Text>
+        <Text fontSize="2rem" fontWeight="600">
+          {' '}
+          ${data.revenue}:{' '}
+        </Text>
+      </Flex>
+
+      <CircularProgress value={40} color="green.400" size="5rem" thickness="10px">
+        <CircularProgressLabel color={'gray.500'} fontSize=".9rem">
+          {' '}
+          40%{' '}
+        </CircularProgressLabel>
+      </CircularProgress>
+    </Flex>
+  )
+}
+
+export default Widget

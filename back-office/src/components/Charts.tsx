567316a867a39f821ed4a233fccf4780479c7718
Samy Senani <samy.senani@deepor.ai>
2022-03-22 11:13:15 +0100

    edit: chart cpt add: select option --> interactive chart sorted (daily - weekly - monthly - yearly)

--- back-office/src/components/Charts.tsx
+++ back-office/src/components/Charts.tsx
@@ -1,57 +1,51 @@
 import React, { useEffect, useState } from 'react'
-import { Flex, useColorMode } from '@chakra-ui/react'
+import { Flex, useColorMode, Box } from '@chakra-ui/react'
 import ReactApexChart from 'react-apexcharts'
 
 interface IChart {
   type: string
   options: any
+  series: any
+  sorted: string
 }
-function Charts({ type = 'area', options }: IChart) {
+function Charts({ type = 'area', options, series, sorted }: IChart) {
   const { colorMode: mode } = useColorMode()
-  // const mode = 'dark'
-  console.log('mode: ', mode)
-
-  const [optionsChart, setOptionsCart] = useState(options)
 
   const Chart =
     type === 'area' ? (
       <ReactApexChart
         type={'area'}
-        options={{ ...optionsChart.options, theme: { mode: mode } }}
-        series={optionsChart.series}
+        options={{ ...options, theme: { mode: mode } }}
+        series={series}
         height={350}
       />
     ) : type === 'line' ? (
       <ReactApexChart
         type={'line'}
-        options={{ ...optionsChart.options, theme: { mode: mode } }}
-        series={optionsChart.series}
+        options={{ ...options, theme: { mode: mode } }}
+        series={series}
         height={350}
-        width={400}
       />
     ) : type === 'bar' ? (
       <ReactApexChart
         type={'bar'}
-        options={{ ...optionsChart.options, theme: { mode: mode } }}
-        series={optionsChart.series}
+        options={{ ...options, theme: { mode: mode } }}
+        series={series}
         height={350}
-        width={400}
       />
     ) : type === 'donut' ? (
       <ReactApexChart
         type={'donut'}
-        options={{ ...optionsChart.options, theme: { mode: mode } }}
-        series={optionsChart.series}
+        options={{ ...options, theme: { mode: mode } }}
+        series={series}
         height={350}
-        width={400}
       />
     ) : type === 'pie' ? (
       <ReactApexChart
         type={'pie'}
-        options={{ ...optionsChart.options, theme: { mode: mode } }}
-        series={optionsChart.series}
+        options={{ ...options, theme: { mode: mode } }}
+        series={series}
         height={350}
-        width={400}
       />
     ) : (
       ''
@@ -69,15 +63,22 @@ function Charts({ type = 'area', options }: IChart) {
 
   return (
     <Flex
-      flexDir="row"
-      justifyContent={'space-between'}
-      alignItems="center"
+      flexDir="column"
       borderColor="gray.400"
       p=".5rem 1rem"
-      w="30em"
+      maxW="30em"
       borderRadius={'25px'}
       boxShadow="md"
+      mb="1.5rem"
+      flexBasis="20rem"
     >
+      <Box mb="1rem" ml="auto" color={'gray.400'}>
+        sorted:{' '}
+        <Box as="span" textTransform={'uppercase'}>
+          {' '}
+          {sorted}{' '}
+        </Box>
+      </Box>
       {Chart}
     </Flex>
   )
--- back-office/src/pages/Analytics.tsx
+++ back-office/src/pages/Analytics.tsx
@@ -1,8 +1,9 @@
-import { Heading, Flex } from '@chakra-ui/react'
+import { Heading, Flex, Button, Stack, Select } from '@chakra-ui/react'
 // import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { Layout, Widget, TotalRevenue, Charts } from 'components'
+import { useState } from 'react'
 
 // chart options
 const chartOptions = {
@@ -10,6 +11,7 @@ const chartOptions = {
     color: ['#6ab04c', '#2980b9'],
     chart: {
       height: 350,
+      width: 450,
       zoom: {
         enabled: true
       }
@@ -17,50 +19,101 @@ const chartOptions = {
   },
   series: [
     {
-      name: 'All Tasks',
-      data: [31, 40, 28, 51, 42, 109, 100]
+      name: 'Online Customers',
+      data: [40, 70, 20, 90, 36, 80, 30, 91, 60]
     },
     {
-      name: 'My Tasks',
-      data: [11, 32, 45, 32, 34, 52, 41]
+      name: 'Store Customers',
+      data: [40, 30, 70, 80, 40, 16, 40, 20, 51, 10]
     }
   ]
 }
+const widgetsData = [
+  {
+    name: 'name...',
+    description: 'description...',
+    revenue: 1000,
+    percent: 40
+  },
+  {
+    name: 'name...',
+    description: 'description...',
+    revenue: 1000,
+    percent: 35
+  },
+  {
+    name: 'name...',
+    description: 'description...',
+    revenue: 1000,
+    percent: 55
+  }
+]
+const totalRevenueData = {
+  percent: 50,
+  price: 5000
+}
 export default function Analytics() {
-  const data = [
-    {
-      name: 'name...',
-      description: 'description...',
-      revenue: 1000
-    },
-    {
-      name: 'name...',
-      description: 'description...',
-      revenue: 1000
-    },
-    {
-      name: 'name...',
-      description: 'description...',
-      revenue: 1000
-    }
-  ]
+  const [options, setOptions] = useState(chartOptions.options)
+  const [series, setSeries] = useState(chartOptions.series)
+  const [sorted, setSorted] = useState('daily')
 
-  const { t } = useTranslation()
+  // const { t } = useTranslation()
+
+  const handleClick = (e: any) => {
+    const category = e.target.value.toLowerCase()
+    // console.log('category: ', category)
+    let min = 2
+    // use certain formula according to the case
+    if (category === 'daily') {
+      min = 10
+      setSorted(category)
+    } else if (category === 'weekly') {
+      min = 1
+      setSorted(category)
+    } else if (category === 'monthly') {
+      min = -1
+      setSorted(category)
+    } else if (category === 'yearly') {
+      min = 0
+      setSorted(category)
+    }
+
+    setSeries(prev =>
+      series.map(s => {
+        const data = s.data.map(() => {
+          return Math.floor(Math.random() * (180 - min + 1)) + min
+        })
+        return { data, name: s.name }
+      })
+    )
+  }
 
   return (
     <Layout isHeaderVisible>
       <Heading as="h2" fontSize="24px">
         Analytics
       </Heading>
-      <Flex flexWrap="wrap" justifyContent={'space-between'}>
-        {data?.map((el: any) => (
+      <Flex flexWrap="wrap" justifyContent={'space-evenly'}>
+        {widgetsData?.map((el: any) => (
           <Widget data={el} />
         ))}
 
-        <TotalRevenue />
-        <Charts type={'line'} options={chartOptions} />
-        <Charts type={'bar'} options={chartOptions} />
-        <Charts type={'area'} options={chartOptions} />
+        <TotalRevenue data={totalRevenueData} />
+
+        <Flex flexDir="column">
+          <Select onChange={handleClick} defaultValue={sorted} w="6.5rem" p="0" ml="auto">
+            <option value="daily"> Daily </option>
+            <option value="weekly"> Weekly </option>
+            <option value="monthly"> Monthly </option>
+            <option value="yearly"> yearly </option>
+          </Select>
+
+          <Flex justifyContent={'space-evenly'} flexWrap="wrap">
+            <Charts type={'line'} options={options} series={series} sorted={sorted} />
+            <Charts type={'area'} options={options} series={series} sorted={sorted} />
+            <Charts type={'bar'} options={options} series={series} sorted={sorted} />
+          </Flex>
+        </Flex>
       </Flex>
 
       {/* {t('greeting')} */}

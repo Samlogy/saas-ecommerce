af1eb4350b6e4e47a7f15e6af126a39c10fb27a1
Samy Senani <samy.senani@deepor.ai>
2022-04-19 12:37:31 +0200

    fix: step form cpt --> data validation for each step

--- front/components/StepForm.tsx
+++ front/components/StepForm.tsx
@@ -3,24 +3,34 @@ import { Flex, Box, Button } from '@chakra-ui/react'
 import { useRef, useState } from 'react'
 import { ModalPopUp } from '../components'
 
+{
+  /* <StepForm steps={steps} handleSubmit={handleSubmit} trigger={trigger} onSubmit={onSubmit} /> */
+}
+
 interface IStepFrom {
   initialStep?: number
   steps: any
-  handleForm?: any
+  handleSubmit?: any
+  trigger?: any
+  onSubmit?: any
 }
 
-const StepForm = ({ initialStep = 0, steps, handleForm }: IStepFrom) => {
+const StepForm = ({ initialStep = 0, steps, handleSubmit, trigger, onSubmit }: IStepFrom) => {
   const { nextStep, prevStep, activeStep } = useSteps({
     initialStep: initialStep
   })
 
-  const onSubmit = () => {
-    console.log('form submitted !')
-  }
-
-  const handleSubmit = () => {
+  const handleSubmitt = async () => {
     btnRef?.current?.click()
     setShow({ state: true, status: 'success' })
+  }
+
+  const handleNextStep = async () => {
+    let isValid = false
+    for (let i = 0; i <= activeStep; i++) {
+      if (i === activeStep) isValid = await trigger(steps[activeStep].fields)
+    }
+    if (isValid) nextStep()
   }
 
   const [show, setShow] = useState({ state: false, status: '' })
@@ -29,35 +39,22 @@ const StepForm = ({ initialStep = 0, steps, handleForm }: IStepFrom) => {
   return (
     <>
       <Box my="2rem">
-        <Steps activeStep={activeStep}>
-          {steps.map(({ label, content, icon, description }) => (
-            <Step
-              label={label}
-              key={label}
-              icon={icon}
-              description={description}
-              _hover={{ cursor: 'pointer' }}
-            >
-              <form onSubmit={handleForm}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Steps activeStep={activeStep}>
+            {steps.map(({ label, icon, content, description }) => (
+              <Step label={label} key={label} icon={icon} description={description}>
                 {content}
-                <Button
-                  type="submit"
-                  mx="auto"
-                  size="sm"
-                  onClick={() => onSubmit()}
-                  ref={btnRef}
-                  display="none"
-                >
-                  Hidden
-                </Button>
-              </form>
-            </Step>
-          ))}
-        </Steps>
+              </Step>
+            ))}
+            <Button type="submit" mx="auto" size="sm" ref={btnRef} display="none">
+              Hidden
+            </Button>
+          </Steps>
+        </form>
 
         {activeStep === steps.length ? (
           <Flex p={4}>
-            <Button mx="auto" size="sm" onClick={() => handleSubmit()}>
+            <Button mx="auto" size="sm" onClick={() => handleSubmitt()}>
               Submit
             </Button>
           </Flex>
@@ -72,7 +69,7 @@ const StepForm = ({ initialStep = 0, steps, handleForm }: IStepFrom) => {
             >
               Prev
             </Button>
-            <Button size="sm" onClick={nextStep}>
+            <Button size="sm" onClick={() => handleNextStep()}>
               {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
             </Button>
           </Flex>

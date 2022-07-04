3420c618a3035c7dbeacfdd9f8934bbd52f5375f
samy senani <aceportgasd235@gmail.com>
2022-07-04 19:36:56 +0100

    add: pagination --> listing comments

--- client/components/ListingComments.tsx
+++ client/components/ListingComments.tsx
@@ -1,13 +1,19 @@
 import { Button, Flex, Heading, Text, useColorModeValue } from '@chakra-ui/react'
 import React, { useState } from 'react'
-import { AddComment, Comment, View } from '../components'
+import { AddComment, Comment, View, Pagination } from '../components'
 import { IComment } from '../lib/interfaces'
 import { GET_ALL_COMMENTS } from '../lib/services'
 import { useAuth } from '../store'
 
 const ListingComments = ({ comments }: { comments: IComment[] }) => {
+  // pagination logic
+  const [page, setPage] = useState<number>(1)
+  const data = { info: { pages: 3 } }
+
   const [showAddComment, setShowAddComment] = useState(false)
+
   const textColor = useColorModeValue('balck', 'white')
+
   const isLogged = useAuth((state: any) => state.isLogged)
 
   // load related products (apollo --> API) GET_ALL_COMMENTS
@@ -35,8 +41,20 @@ const ListingComments = ({ comments }: { comments: IComment[] }) => {
 
       <View cond={comments?.length > 0}>
         {comments?.map(comment => (
-          <Comment data={comment} />
+          <Comment key={comment?.id} data={comment} />
         ))}
+
+        <Pagination
+          page={page}
+          pages={[page, page + 1, page + 2, page + 3]}
+          changePage={setPage}
+          nextPage={() => setPage(prev => prev + 1)}
+          prevPage={() => setPage(prev => prev - 1)}
+          startPage={() => setPage(1)}
+          endPage={() => setPage(data.info.pages)}
+          lastPage={data.info.pages}
+          isMobile={true}
+        />
       </View>
 
       <View cond={comments?.length === 0}>

4fa5dde7d48799b4f329a05fdffd34f9c7f749a3
samy senani <aceportgasd235@gmail.com>
2022-07-10 09:25:47 +0100

    refactoring: ordering cpts add: custom fileds (input, text, select)

---
+++ back-office/src/components/SelectField.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Select, FormControl, FormLabel } from '@chakra-ui/react'
+import { ErrorMessage } from '../components'
+
+interface ISelectField {
+  errors?: any
+  register?: any
+  name: string
+  label?: string
+  placeholder?: string
+  children: any
+  icon?: any
+  [restProps: string]: any
+}
+
+export default function SelectField({
+  errors,
+  register,
+  name,
+  label,
+  placeholder,
+  icon,
+  children,
+  ...restProps
+}: ISelectField) {
+  const width = restProps.w ? restProps.w : '20rem'
+  return (
+    <FormControl id={name} mb=".5rem" w={width}>
+      {label && <FormLabel> {label} </FormLabel>}
+      {register ? (
+        <Select
+          placeholder={placeholder}
+          _placeholder={{ color: 'gray_4' }}
+          isInvalid={errors[name] && register ? true : false}
+          focusBorderColor={errors[name] && register ? 'error' : 'accent_5'}
+          borderRadius="5px"
+          icon={icon && icon}
+          {...register(name)}
+          {...restProps}
+        >
+          {children}
+        </Select>
+      ) : (
+        <Select
+          placeholder={placeholder}
+          _placeholder={{ color: 'gray_4' }}
+          focusBorderColor={'accent_5'}
+          borderRadius="5px"
+          icon={icon && icon}
+          {...restProps}
+        >
+          {children}
+        </Select>
+      )}
+      {register && <ErrorMessage error={errors[name]?.message} />}
+    </FormControl>
+  )
+}
--- back-office/src/components/index.ts
+++ back-office/src/components/index.ts
@@ -1,28 +1,40 @@
-export { default as View } from './View'
+export { default as Widget } from './Widget'
+export { default as TotalRevenue } from './TotalRevenue'
+export { default as Dropdown } from './Dropdown'
+export { default as Charts } from './Charts'
+
+export { default as CustomTable } from './CustomTable'
+export { default as LanguageSwitcher } from './LanguageSwitcher'
+export { default as CustomPreLoader } from './CustomPreLoader'
+export { default as PreviewImages } from './PreviewImages'
+
+// global components
+export { default as TopBar } from './TopBar'
+export { default as SideBar } from './SideBar'
 export { default as Layout } from './Layout'
+export { default as View } from './View'
+export { default as ProtectedRoute } from './ProtectedRoute'
+export { default as LoginButton } from './LoginButton'
+export { default as LogoutButton } from './LogoutButton'
+export { default as DarkModeToggle } from './DarkModeToggle'
+
+// custom fields
+export { default as InputField } from './InputField'
+export { default as TextField } from './TextField'
+export { default as SelectField } from './SelectField'
 export { default as ErrorMessage } from './ErrorMessage'
 
+// products
 export { default as ProductsFilter } from './ProductsFilter'
 export { default as ActionsMenu } from './ActionsMenu'
 export { default as ProductBox } from './ProductBox'
 export { default as ProductDetails } from './ProductDetails'
 export { default as AddEditProduct } from './AddEditProduct'
-export { default as ProtectedRoute } from './ProtectedRoute'
-export { default as LoginButton } from './LoginButton'
-export { default as LogoutButton } from './LogoutButton'
+
+// notifications
+export { default as NotificationButton } from './NotificationButton'
+export { default as NotificationDetails } from './NotificationDetails'
+
+// messages
 export { default as MessageButton } from './MessageButton'
 export { default as MessageDetails } from './MessageDetails'
-
-export { default as SideBar } from './SideBar'
-export { default as TopBar } from './TopBar'
-export { default as DarkModeToggle } from './DarkModeToggle'
-export { default as NotificationButton } from './NotificationButton'
-export { default as Widget } from './Widget'
-export { default as TotalRevenue } from './TotalRevenue'
-export { default as Dropdown } from './Dropdown'
-export { default as Charts } from './Charts'
-export { default as NotificationDetails } from './NotificationDetails'
-export { default as CustomTable } from './CustomTable'
-export { default as LanguageSwitcher } from './LanguageSwitcher'
-export { default as CustomPreLoader } from './CustomPreLoader'
-export { default as PreviewImages } from './PreviewImages'

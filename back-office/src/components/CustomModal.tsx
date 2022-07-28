c6d903ed7cec0748a95f259af4390f6d816e05a1
samy senani <aceportgasd235@gmail.com>
2022-07-28 11:27:24 +0100

    edit: modal custom cpt

--- back-office/src/components/CustomModal.tsx
+++ back-office/src/components/CustomModal.tsx
@@ -12,13 +12,21 @@ interface ICustomModal {
   isOpen: boolean
   onClose: any
   title?: string
+  size?: any
   body?: React.ReactNode
   footer?: React.ReactNode
 }
 
-export default function CustomModal({ isOpen, onClose, title, body, footer }: ICustomModal) {
+export default function CustomModal({
+  isOpen,
+  onClose,
+  title,
+  size = 'md',
+  body,
+  footer
+}: ICustomModal) {
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={onClose} size={size}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader> {title && title} </ModalHeader>

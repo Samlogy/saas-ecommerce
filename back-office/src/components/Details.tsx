760e3872a7a84c3b68abf8c6c472f7fb2f7a0731
samy senani <aceportgasd235@gmail.com>
2022-07-28 11:27:46 +0100

    edit: details cpt --> notifs / msgs details

--- back-office/src/components/Details.tsx
+++ back-office/src/components/Details.tsx
@@ -1,64 +1,50 @@
-import {
-  Button,
-  Flex,
-  Image,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  Text
-} from '@chakra-ui/react'
+import { Button, Flex, Image, Text } from '@chakra-ui/react'
+
+import { CustomModal } from './'
 
 interface IDetails {
   title: string
   data: any
   isOpen: boolean
-  setOpen: (isOpen: boolean) => void
+  onClose: any
 }
 
-export default function Details({ title, data, isOpen, setOpen }: IDetails) {
+export default function Details({ title, data, isOpen, onClose }: IDetails) {
+  const Body = (
+    <Flex flexDir="column">
+      <Image
+        src={data?.img}
+        alt="message-image"
+        borderRadius={'5px'}
+        w="5rem"
+        h="5rem"
+        mb=".5rem"
+      />
+      <Text mb=".5rem" fontSize="1.3rem">
+        {data?.title}{' '}
+      </Text>
+      <Text mb=".5rem" color="gray_2">
+        {data?.text}{' '}
+      </Text>
+      <Text fontSize=".8rem" fontStyle="italic" textAlign={'right'} color="gray_4">
+        {data?.editedAt || data?.createdAt}{' '}
+      </Text>
+    </Flex>
+  )
+  const Footer = (
+    <Button bg={'gray_4'} color="white" _hover={{ bg: 'gray_3' }} onClick={onClose}>
+      Close
+    </Button>
+  )
+  console.log(isOpen)
   return (
-    <Modal isOpen={isOpen} onClose={() => setOpen(false)} size="xl">
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader> {title} </ModalHeader>
-        <ModalCloseButton />
-
-        <ModalBody>
-          <Flex flexDir="column">
-            <Image
-              src={data?.img}
-              alt="message-image"
-              borderRadius={'5px'}
-              w="5rem"
-              h="5rem"
-              mb=".5rem"
-            />
-            <Text mb=".5rem" fontSize="1.3rem">
-              {data?.title}{' '}
-            </Text>
-            <Text mb=".5rem" color="gray_2">
-              {data?.text}{' '}
-            </Text>
-            <Text fontSize=".8rem" fontStyle="italic" textAlign={'right'} color="gray_4">
-              {data?.editedAt || data?.createdAt}{' '}
-            </Text>
-          </Flex>
-        </ModalBody>
-        <ModalFooter>
-          <Button
-            bg={'gray_4'}
-            color="white"
-            _hover={{ bg: 'gray_3' }}
-            onClick={() => setOpen(false)}
-          >
-            Close{' '}
-          </Button>{' '}
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
+    <CustomModal
+      isOpen={isOpen}
+      onClose={onClose}
+      size="xl"
+      title={title}
+      body={Body}
+      footer={Footer}
+    />
   )
 }

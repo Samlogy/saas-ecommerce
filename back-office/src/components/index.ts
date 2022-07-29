752e51d9e59d15ce39cc60beb16b971896f29d7f
samy senani <aceportgasd235@gmail.com>
2022-07-29 08:08:01 +0100

    delete: useless code

--- back-office/src/components/index.ts
+++ back-office/src/components/index.ts
@@ -43,7 +43,6 @@ export { default as CustomChart } from './CustomChart'
 
 // other
 export { default as CustomTable } from './CustomTable'
-export { default as PreviewImages } from './PreviewImages'
 export { default as CustomDrawer } from './CustomDrawer'
 export { default as CustomModal } from './CustomModal'
 export { default as DisplayRowData } from './DisplayRowData'

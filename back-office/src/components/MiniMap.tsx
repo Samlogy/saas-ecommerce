edf1fba7d7c09ed15753ba71ba394f09004c18c5
samy senani <aceportgasd235@gmail.com>
2022-08-25 20:34:50 +0100

    edit: mini map cpt --> as mini menu

--- back-office/src/components/MiniMap.tsx
+++ back-office/src/components/MiniMap.tsx
@@ -1,4 +1,18 @@
-import { Flex, Link as ChakraLink, useColorModeValue } from '@chakra-ui/react'
+import {
+  Flex,
+  Link as ChakraLink,
+  useColorModeValue,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+  Button
+} from '@chakra-ui/react'
+import { AiOutlineMenu } from 'react-icons/ai'
 
 import img from '../assets/images/home.png'
 
@@ -67,29 +81,28 @@ export default function MiniMap({ data }: { data: any }) {
   const bgColor = useColorModeValue('gray_9', 'gray_2')
   const textColor = useColorModeValue('gray_3', 'gray_9')
   return (
-    <Flex
-      flexDir={'column'}
-      w="12rem"
-      h="fit-content"
-      border="1px solid"
-      borderColor={bgColor}
-      bg={bgColor}
-      color={textColor}
-      p=".5rem"
-      borderRadius={'10px'}
-      flexBasis={['20%', '', '', '10%']}
-      display={['none', '', 'flex']}
-    >
-      {data.map((el: any, idx: number) => (
-        <ChakraLink
-          key={idx}
-          href={el.link}
-          _hover={{ color: 'accent_3', textDecor: 'underline' }}
-          mb=".25rem"
-        >
-          {el.label}
-        </ChakraLink>
-      ))}
-    </Flex>
+    <Menu>
+      <MenuButton
+        as={Button}
+        rightIcon={<AiOutlineMenu />}
+        bg={bgColor}
+        color={textColor}
+        _hover={{ bg: bgColor }}
+      >
+        Menu
+      </MenuButton>
+      <MenuList>
+        {data.map((el: any, idx: number) => (
+          <ChakraLink
+            key={idx}
+            href={el.link}
+            _hover={{ color: 'accent_3', textDecor: 'underline' }}
+            mb=".25rem"
+          >
+            <MenuItem>{el.label}</MenuItem>
+          </ChakraLink>
+        ))}
+      </MenuList>
+    </Menu>
   )
 }

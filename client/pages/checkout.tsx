5addade7d91c6aaa535cca89b82cae2c0b0f574a
samy senani <aceportgasd235@gmail.com>
2022-09-27 11:03:45 +0100

    add: Review Order cpt --> step in checkout process

--- client/pages/checkout.tsx
+++ client/pages/checkout.tsx
@@ -1,22 +1,39 @@
-import { Box, Checkbox, Flex, FormLabel, Heading, Radio, RadioGroup } from '@chakra-ui/react'
-import { yupResolver } from '@hookform/resolvers/yup'
+import {
+  Box,
+  Button,
+  Checkbox,
+  Divider,
+  Flex,
+  FormLabel,
+  GridItem,
+  Heading,
+  HStack,
+  Image,
+  Radio,
+  RadioGroup,
+  SimpleGrid,
+  Text,
+  useBreakpointValue,
+  useColorMode,
+  useColorModeValue,
+  VStack
+} from '@chakra-ui/react'
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
+import { MdOutlineLocalShipping, MdOutlinePayment } from 'react-icons/md'
 import { RiBillLine } from 'react-icons/ri'
-import { MdOutlinePayment, MdOutlineLocalShipping } from 'react-icons/md'
 
 import {
+  CheckoutForm,
   ErrorMessage,
   FeedBack,
   InputField,
   Layout,
   StepForm,
-  TextField,
-  CheckoutForm
+  TextField
 } from '../components'
-import { checkoutchema } from '../lib/validation'
 
 export default function Checkout() {
   const [feedBack, setFeedBack] = useState({
@@ -83,9 +100,9 @@ export default function Checkout() {
       )
     },
     {
-      label: 'Payment',
-      icon: MdOutlinePayment,
-      content: <Payment setFeedBack={setFeedBack} />
+      label: 'Review Order',
+      icon: MdOutlineLocalShipping,
+      content: <ReviewOrder />
     }
   ]
 
@@ -236,11 +253,165 @@ function ShippingMethod({
     </Flex>
   )
 }
-function Payment({ setFeedBack }: { setFeedBack: any }) {
-  const price = '25£' // get price to pay
+
+function ReviewOrder() {
   return (
-    <Flex flexDir="column" align="center" justify="center" m="3rem 0 1rem 0">
-      <CheckoutForm price={price} setFeedBack={setFeedBack} />
+    <Flex
+      justify={['center', '', 'space-between', 'space-around']}
+      align="start"
+      flexWrap="wrap"
+      py={[0, 10, 20]}
+      w={['100%']}
+    >
+      <Details />
+      <Cart />
     </Flex>
+  )
+}
+
+const Cart = () => {
+  const { toggleColorMode } = useColorMode()
+
+  const bgColor = useColorModeValue('gray.50', 'whiteAlpha.50')
+  const secondaryTextColor = useColorModeValue('gray.600', 'gray.400')
+
+  // computing all these fct get data from store (product store)
+
+  const products = useShoppingCartStore((state: any) => state.products)
+
+  const computeSubTotal: number = useMemo(() => {
+    return 119
+  }, [])
+  const computeShippingFee: number = useMemo(() => {
+    return 100
+  }, [])
+  const computeTaxes: number = useMemo(() => {
+    const TVA = 0.1
+    const newPrice = computeSubTotal * TVA
+    return newPrice
+  }, [])
+  const computeTotal: number = useMemo(() => {
+    return computeSubTotal + computeShippingFee + computeTaxes
+  }, [])
+  return (
+    <VStack w={['100%', '50%', '30em']} align="flex-start" h="full" p={10} spacing={6} bg={bgColor}>
+      <VStack alignItems="flex-start" spacing={3}>
+        <Heading size="xl">Your cart</Heading>
+
+        <Text>
+          If the price is too hard on your eyes,{' '}
+          <Button onClick={toggleColorMode} variant="link" colorScheme="black">
+            try changing the theme.
+          </Button>
+        </Text>
+      </VStack>
+
+      <Flex flexDir="column" alignItems="stretch" w="full">
+        {products.map((product: any, idx: number) => (
+          <Flex justify="space-between" key={idx} my=".5em">
+            <Image
+              boxSize="100px"
+              src={product?.images[0]}
+              borderRadius="5px"
+              borderColor="gray"
+              alt="Product"
+            />
+            <Flex flexDir="column" justify="flex-start" align="start">
+              <Text fontSize="1rem" fontWeight="600" textAlign={'center'} mb=".5rem">
+                {product?.name}
+              </Text>
+              <Text fontSize="1rem" fontWeight="600" textAlign={'center'} mb=".5rem">
+                {`x ${product?.quantity}`}
+              </Text>
+
+              <Flex justifyContent={'space-evenly'} alignItems="center">
+                <Box
+                  as="span"
+                  fontStyle="italic"
+                  textDecor={product.discount && 'line-through'}
+                  fontSize=".9rem"
+                >
+                  {`£${product?.quantity * product?.price}`}
+                </Box>
+                <Box as="span" fontWeight="600" fontSize=".9rem">
+                  {product.discount && `£ ${product?.quantity * product?.price * product.discount}`}
+                </Box>
+              </Flex>
+            </Flex>
+          </Flex>
+        ))}
+      </Flex>
+
+      <VStack spacing={4} alignItems="stretch" w="full">
+        <HStack justifyContent="space-between">
+          <Text color={secondaryTextColor}>Subtotal</Text>
+          <Heading size="sm"> {`£${computeSubTotal}`} </Heading>
+        </HStack>
+        <HStack justifyContent="space-between">
+          <Text color={secondaryTextColor}>Shipping</Text>
+          <Heading size="sm">{`£${computeShippingFee}`}</Heading>
+        </HStack>
+        <HStack justifyContent="space-between">
+          <Text color={secondaryTextColor}>Taxes (estimated)</Text>
+          <Heading size="sm">{`£${computeTaxes}`}</Heading>
+        </HStack>
+      </VStack>
+      <Divider />
+      <HStack justifyContent="space-between" w="full">
+        <Text color={secondaryTextColor}>Total</Text>
+        <Heading size="lg">{`£${computeTotal}`}</Heading>
+      </HStack>
+    </VStack>
+  )
+}
+const Details = () => {
+  const colSpan = useBreakpointValue({ base: 2, md: 1 })
+
+  const bgColor = useColorModeValue('gray.50', 'whiteAlpha.50')
+  const secondaryTextColor = useColorModeValue('gray.600', 'gray.400')
+
+  const labels = [
+    'full name',
+    'email',
+    'phone',
+    'address',
+    'zip code',
+    'shipping address',
+    'shipping method'
+  ]
+  const values = [
+    'full name',
+    'email',
+    'phone',
+    'address',
+    'zip code',
+    'shipping address',
+    'shipping method'
+  ]
+  return (
+    <VStack
+      w={['100%', '50%', '30em']}
+      align="flex-start"
+      h="full"
+      p={10}
+      spacing={10}
+      bg={bgColor}
+    >
+      <VStack spacing={2} alignItems="flex-start">
+        <Heading size="xl">Your details</Heading>
+        <Text>If you already have an account, click here to log in.</Text>
+      </VStack>
+
+      <SimpleGrid columns={1} rowGap={5} w="full">
+        {labels.map((el, idx) => (
+          <GridItem key={idx} colSpan={colSpan} textTransform="capitalize">
+            <Box as="span"> {labels[idx]}: </Box>
+            <Box as="span" fontStyle="italic" color={secondaryTextColor}>
+              {values[idx]}
+            </Box>
+          </GridItem>
+        ))}
+      </SimpleGrid>
+    </VStack>
   )
 }

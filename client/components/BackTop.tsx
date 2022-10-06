e9e94bd27343a3e557ccccc47a320e70ff47e23a
samy senani <aceportgasd235@gmail.com>
2022-10-06 09:19:17 +0100

    fix: back top button --> layout

--- client/components/BackTop.tsx
+++ client/components/BackTop.tsx
@@ -1,8 +1,8 @@
-import { IconButton } from '@chakra-ui/react'
+import { IconButton, useColorModeValue } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
-import { FaArrowUp } from 'react-icons/fa'
+import { BiUpArrowAlt } from 'react-icons/bi'
 
-export default function BackTop() {
+export default function BackToTop() {
   const [showButton, setShowButton] = useState(false)
 
   useEffect(() => {
@@ -22,20 +22,30 @@ export default function BackTop() {
       behavior: 'smooth' // for smoothly scrolling
     })
   }
+  const bgColor = useColorModeValue('gray_7', 'gray_2')
+  const bgHoverColor = useColorModeValue('gray_6', 'gray_3')
+  const textColor = useColorModeValue('gray_1', 'gray_7')
 
   return (
     <>
       {showButton && (
         <IconButton
           aria-label="scroll-top"
-          icon={<FaArrowUp size={20} color="white" />}
+          icon={<BiUpArrowAlt size={26} />}
           onClick={scrollToTop}
-          bg="accent_1"
-          _hover={{ bg: 'accent_2', cursor: 'pointer' }}
+          color={textColor}
+          bg={bgColor}
+          transition="all .35s"
+          _hover={{
+            bg: bgHoverColor,
+            cursor: 'pointer',
+            boxShadow: '0px 0px 50px 2px rgb(0 0 0 / 25%)'
+          }}
           position="fixed"
-          right="20px"
-          bottom="20px"
+          right="2%"
+          bottom="8%"
           w="1rem"
+          zIndex="100"
           borderRadius={'10px'}
         />
       )}

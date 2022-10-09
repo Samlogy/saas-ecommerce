13c9ccee026c102271fda6bef7014c2d09fb15ba
samy senani <aceportgasd235@gmail.com>
2022-10-09 10:42:40 +0100

    edit: stripe form cpt *add: card nbr element - card epiry element - card cvc element

--- client/components/StripeForm.tsx
+++ client/components/StripeForm.tsx
@@ -1,5 +1,14 @@
 import { Button, Flex, FormControl, FormLabel } from '@chakra-ui/react'
-import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
+import {
+  CardElement,
+  useElements,
+  useStripe,
+  CardNumberElement,
+  CardCvcElement,
+  CardExpiryElement,
+  Elements,
+  ElementsConsumer
+} from '@stripe/react-stripe-js'
 import { useState } from 'react'
 import ErrorMessage from './ErrorMessage'
 
@@ -16,8 +25,19 @@ export default function StripeForm({ price, billingDetails, setFeedBack }: IStri
     complete: false,
     isLoading: false,
     paymentMethod: null,
-    error: null
+    errors: null
   })
+
+  const onReset = () => {
+    setCard(prev => {
+      return {
+        ...prev,
+        errors: null,
+        isLoading: false,
+        paymentMethod: null
+      }
+    })
+  }
 
   const onSubmit = async e => {
     e.preventDefault()
@@ -28,7 +48,7 @@ export default function StripeForm({ price, billingDetails, setFeedBack }: IStri
       return
     }
 
-    if (card?.error) {
+    if (card?.errors) {
       elements.getElement('card').focus()
       return
     }
@@ -75,33 +95,80 @@ export default function StripeForm({ price, billingDetails, setFeedBack }: IStri
     }
   }
 
-  const onReset = () => {
+  const handlePayment_InputChange = (name, label) => elementData => {
+    let errors = card?.errors || {}
+
+    if (!elementData.complete && !elementData.error) {
+      errors[name] = `Your ${label} is incomplete or invalid.`
+    } else if (elementData.complete && !elementData.error) {
+      delete errors[name]
+    }
+
+    //console.log(elementData, name, label)
+
     setCard(prev => {
       return {
         ...prev,
-        error: null,
-        isLoading: false,
-        paymentMethod: null
+        errors
       }
     })
   }
 
-  const onHandleCard = (e: any) => {
-    setCard(prev => {
-      return {
-        ...prev,
-        error: e.error,
-        complete: e.complete
+  const StripeElementsStyles = {
+    iconStyle: 'solid',
+    style: {
+      base: {
+        iconColor: 'black',
+        color: 'black',
+        fontWeight: 300,
+        fontFamily: "'Lato', 'Helvetica Neue', Arial, Helvetica, sans-serif",
+        fontSize: '16px',
+        backgroundColor: 'white',
+        fontSmoothing: 'antialiased',
+        ':-webkit-autofill': {
+          color: '#fce883'
+        },
+        '::placeholder': {
+          color: 'lightgrey'
+        }
+      },
+      invalid: {
+        iconColor: 'red',
+        color: 'red'
       }
-    })
+    }
   }
 
   return (
     <form onSubmit={onSubmit}>
-      <FormControl id="cardInfo" mb="1em" w="20em">
-        <FormLabel htmlFor="cardInfo">Card</FormLabel>
-        <CardElement className="sr-input sr-card-element" onChange={onHandleCard} />
-        <ErrorMessage error={card?.error?.mesage} />
+      <FormControl id="cardNumber" mb="1em" w="20em">
+        <FormLabel htmlFor="cardNumber">Card Number</FormLabel>
+        <CardNumberElement
+          id="cardNumber"
+          onChange={handlePayment_InputChange('cardNumber', 'Card Number')}
+          options={StripeElementsStyles}
+        />
+        <ErrorMessage error={card?.errors?.mesage} />
+      </FormControl>
+
+      <FormControl id="cardExpiration" mb="1em" w="20em">
+        <FormLabel htmlFor="cardExpiration">Card Expiration</FormLabel>
+        <CardExpiryElement
+          id="cardExpiry"
+          onChange={handlePayment_InputChange('cardExpiration', 'Card Expiration')}
+          options={StripeElementsStyles}
+        />
+        <ErrorMessage error={card?.errors?.mesage} />
+      </FormControl>
+
+      <FormControl id="cardCVC" mb="1em" w="20em">
+        <FormLabel htmlFor="cardCVC">Card CVC</FormLabel>
+        <CardCvcElement
+          id="cardCvc"
+          onChange={handlePayment_InputChange('cardCVC', 'Card CVC')}
+          options={StripeElementsStyles}
+        />
+        <ErrorMessage error={card?.errors?.mesage} />
       </FormControl>
 
       <Flex justify="flex-end">
